451335a44f871a164f0d873f7ec7befe61ded766
newObjectccc <897333949@qq.com>
2021-07-11 00:03:09 +0800

    feat(components): 重新整理依赖，修改文章详情页头部组件

--- components/articleItem/index.jsx
+++ components/articleItem/index.jsx
@@ -1,6 +1,7 @@
 import { ClockCircleFilled, DatabaseFilled, TagFilled } from '@ant-design/icons'
 import dayjs from 'dayjs'
 import articleItemStyles from './articleItem.module.css'
+import Link from 'next/link'
 
 const relativeTime = require('dayjs/plugin/relativeTime')
 
@@ -8,23 +9,28 @@ dayjs.extend(relativeTime)
 
 const ArticleItem = (props) => {
   return (
-    <div className={articleItemStyles.container}>
-      <div className={articleItemStyles.title}>{props.dataSource.title}</div>
-      <div className={articleItemStyles.content}>{props.dataSource.content}</div>
-      <div className={articleItemStyles.infoWrap}>
-        <ClockCircleFilled />
-        <span>{dayjs(props.dataSource.createTime).fromNow()}</span>
-        <DatabaseFilled />
-        <span>{props.dataSource.categories}</span>
-        <TagFilled />
-        <span>
-          {props.dataSource.tags.map((item, index) =>
-            index + 1 !== props.dataSource.tags.length ? `${item} | ` : `${item}`
-          )}
-        </span>
-        <div className={articleItemStyles.posiMore}>Read more</div>
+    <Link
+      href="/docDetail?id=1287home"
+      key={`articleDocItem${props.dataSource.createTime.toString()}`}
+    >
+      <div className={articleItemStyles.container}>
+        <div className={articleItemStyles.title}>{props.dataSource.title}</div>
+        <div className={articleItemStyles.content}>{props.dataSource.content}</div>
+        <div className={articleItemStyles.infoWrap}>
+          <ClockCircleFilled />
+          <span>{dayjs(props.dataSource.createTime).fromNow()}</span>
+          <DatabaseFilled />
+          <span>{props.dataSource.categories}</span>
+          <TagFilled />
+          <span>
+            {props.dataSource.tags.map((item, index) =>
+              index + 1 !== props.dataSource.tags.length ? `${item} | ` : `${item}`
+            )}
+          </span>
+          <div className={articleItemStyles.posiMore}>Read more</div>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
--- components/avatarInfomation/index.jsx
+++ components/avatarInfomation/index.jsx
@@ -1,5 +1,12 @@
+import avatarInformationStyle from './avatarInformation.module.css'
+
 const AvatarInfomation = (props) => {
-  return <div>{props.articleInfo.title}</div>
+  return (
+    <>
+      <div className={avatarInformationStyle.title}>{props.articleInfo.title}</div>
+      <div className={avatarInformationStyle.avatarWrap}>ccdd's Blog</div>
+    </>
+  )
 }
 
 export default AvatarInfomation
